88ab1a308bb0b5f4f9268aae54b83eb7abac427d
Zhaoyh-Jonathan <jonathan.zhaoyh@gmail.com>
2022-01-11 23:09:07 -0600

    add checkout form shipping addr validators

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -37,11 +37,11 @@ export class CheckoutComponent implements OnInit {
         email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
-        street: [''],
-        city: [''],
-        state: [''],
-        country: [''],
-        zipCode: ['']
+        street: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhiteSpace]),
+        city: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhiteSpace]),
+        state: new FormControl('', [Validators.required]),
+        country: new FormControl('', [Validators.required]),
+        zipCode: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhiteSpace])
       }),
       billingAddress: this.formBuilder.group({
         street: [''],
@@ -93,9 +93,18 @@ export class CheckoutComponent implements OnInit {
 
   }
 
+  // form control getter methods
+
   get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
   get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
   get email() { return this.checkoutFormGroup.get('customer.email'); }
+
+  get shippingAddrStreet() { return this.checkoutFormGroup.get('shippingAddress.street'); }
+  get shippingAddrCity() { return this.checkoutFormGroup.get('shippingAddress.city'); }
+  get shippingAddrState() { return this.checkoutFormGroup.get('shippingAddress.state'); }
+  get shippingAddrCountry() { return this.checkoutFormGroup.get('shippingAddress.country'); }
+  get shippingAddrZipCode() { return this.checkoutFormGroup.get('shippingAddress.zipCode'); }
+  
 
   copyShipping2Billing(event) {
     
